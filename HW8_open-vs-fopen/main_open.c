84f36068a6afe17e6dd053ad5988b160fe69b0ba
ABradRoberts <bradr555@gmail.com>
2019-04-26 18:45:05 -0500

    Started main_open.c

--- HW8_open-vs-fopen/main_open.c
+++ HW8_open-vs-fopen/main_open.c
@@ -1,7 +1,20 @@
 #include <stdio.h>
+#include <fcntl.h>
+#include <unistd.h>
 
 int main()
 {
-    printf("hello world!\n");
+    char *f = "test.txt"; // input file
+    char *o = "caps.txt"; // output file
+
+    int fd = open(f, O_RDONLY);
+
+    void *value;
+    while (read(fd, value, 1) == 0)
+    {
+        printf("%s\n", value);
+    }
+
+    close(fd);
     return 0;
 }
