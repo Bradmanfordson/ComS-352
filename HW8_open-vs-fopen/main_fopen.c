fab3cc6502adab80babb1e070df3d458a0fa92c3
ABradRoberts <bradr555@gmail.com>
2019-04-26 20:26:06 -0500

    Finished.

--- HW8_open-vs-fopen/main_fopen.c
+++ HW8_open-vs-fopen/main_fopen.c
@@ -1,7 +1,58 @@
+// Author: Allen Bradley Roberts
+// Date: April 26, 2019
+// Course: Com S 352 - Operating Systems
+
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
 int main()
 {
+    //TODO ADD CLOCK
+    char *fileIn = malloc(100);
+    char *fileOut = malloc(100);
+
+    printf("Input full path of file to read: ");
+    scanf("%s", fileIn);
+
+    printf("Input full path of file to write: ");
+    scanf("%s", fileOut);
+
+    clock_t start = clock();
+
+    FILE *testIn, *testOut;
+
+    if ((testIn = fopen(fileIn, "r")) == NULL)
+    {
+        printf("An error occured while trying to open: %s.\n", fileIn);
+        printf("Make sure to give the full path of the file.\n");
+        return -1;
+    }
+    if ((testOut = fopen(fileOut, "w")) == NULL)
+    {
+        printf("An error occured while trying to open: %s.\n", fileOut);
+        printf("Make sure to give the full path of the file.\n");
+        return -1;
+    }
+
+    char value;
+    while (fread(&value, 1, 1, testIn) != 0)
+    {
+        if (value < 123 && value > 96) // if value is stricktly a lower case character
+        {
+            value = value - 32;
+        }
+
+        fwrite(&value, 1, 1, testOut);
+    }
+
+    fclose(testIn);
+    fclose(testOut);
+
+    clock_t stop = clock();
+
+    float totalTime = stop - start;
+    printf("Execution time: %f\n", totalTime);
 
     return 0;
 }
--- HW8_open-vs-fopen/main_open.c
+++ HW8_open-vs-fopen/main_open.c
@@ -1,20 +1,64 @@
+// Author: Allen Bradley Roberts
+// Date: April 26, 2019
+// Course: Com S 352 - Operating Systems
+
 #include <stdio.h>
 #include <fcntl.h>
 #include <unistd.h>
+#include <sys/stat.h>
+#include <stdlib.h>
+#include <time.h>
 
 int main()
 {
-    char *f = "test.txt"; // input file
-    char *o = "caps.txt"; // output file
+    char *fileIn = malloc(100);
+    char *fileOut = malloc(100);
 
-    int fd = open(f, O_RDONLY);
+    printf("Input full path of file to read: ");
+    scanf("%s", fileIn);
 
-    void *value;
-    while (read(fd, value, 1) == 0)
+    printf("Input full path of file to write: ");
+    scanf("%s", fileOut);
+
+    clock_t start = clock();
+
+    int inputFD, outputFD;
+
+    if ((inputFD = open(fileIn, O_RDONLY)) == -1)
     {
-        printf("%s\n", value);
+        printf("An error occured while trying to open: %s.\n", fileIn);
+        printf("Make sure to give the full path of the file.\n");
+        return -1;
     }
 
-    close(fd);
+    if ((outputFD = open(fileOut, O_WRONLY)) == -1)
+    {
+        printf("An error occured while trying to open: %s.\n", fileOut);
+        printf("Make sure to give the full path of the file.\n");
+        return -1;
+    }
+
+    struct stat filestat;
+    stat(fileIn, &filestat);
+
+    char value;
+
+    while (read(inputFD, &value, 1) != 0)
+    {
+        if (value < 123 && value > 96) // if value is stricktly a lower case character
+        {
+            value = value - 32;
+        }
+
+        write(outputFD, &value, 1);
+    }
+
+    close(inputFD);
+    close(outputFD);
+
+    clock_t stop = clock();
+
+    float totalTime = stop - start;
+    printf("Execution time: %f\n", totalTime);
     return 0;
 }
